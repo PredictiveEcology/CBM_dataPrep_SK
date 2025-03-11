d56bf7a124e22f225129a89fe78f48d814778eaf
Susan Murray <murray.e.susan@gmail.com>
2025-03-11 12:14:00 -0700

    test set up update

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -20,21 +20,24 @@ spadesTestPaths <- SpaDEStestSetUpDirectories(require = "googledrive")
 
 
 ## Download standard inputs that are usually provided by CBM_defaults or CBM_vol2biomass.
-## RDS data provided where creation of these outputs is more complex than a simple downloads
 
 # Download CBM-CFS3 database usually provided by CBM_defaults
-download.file(
-  url      = "https://raw.githubusercontent.com/cat-cfs/libcbm_py/main/libcbm/resources/cbm_defaults_db/cbm_defaults_v1.2.8340.362.db",
-  destfile = file.path(spadesTestPaths$temp$inputs, "dbPath.db"),
-  mode     = "wb",
-  quiet    = TRUE
-)
+if (!file.exists(file.path(spadesTestPaths$temp$inputs, "cbm_defaults_v1.2.8340.362.db"))){
+  download.file(
+    url      = "https://raw.githubusercontent.com/cat-cfs/libcbm_py/main/libcbm/resources/cbm_defaults_db/cbm_defaults_v1.2.8340.362.db",
+    destfile = file.path(spadesTestPaths$temp$inputs, "cbm_defaults_v1.2.8340.362.db"),
+    mode     = "wb",
+    quiet    = TRUE
+  )
+}
 
 # Download gcMetaEg usually provided by CBM_vol2biomass
-withr::with_options(
-  c(googledrive_quiet = TRUE),
-  googledrive::drive_download(
-    "https://drive.google.com/file/d/189SFlySTt0Zs6k57-PzQMuQ29LmycDmJ",
-    path = file.path(spadesTestPaths$temp$inputs, "gcMetaEg.csv")
-  ))
+if (!file.exists(file.path(spadesTestPaths$temp$inputs, "gcMetaEg.csv"))){
+  withr::with_options(
+    c(googledrive_quiet = TRUE),
+    googledrive::drive_download(
+      "https://drive.google.com/file/d/189SFlySTt0Zs6k57-PzQMuQ29LmycDmJ",
+      path = file.path(spadesTestPaths$temp$inputs, "gcMetaEg.csv")
+    ))
+}
 
