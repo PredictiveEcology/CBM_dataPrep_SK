5e20119003854843f51222923a36c9c35d872a80
Susan Murray <murray.e.susan@gmail.com>
2025-06-06 16:12:13 -0700

    test update

--- tests/testthat/test-2-integration_2-CBM_2-SK.R
+++ tests/testthat/test-2-integration_2-CBM_2-SK.R
@@ -40,15 +40,7 @@ test_that("Integration: CBM: SK 1998-2000", {
       ),
 
       # Set disturbances
-      ## Test matching user disturbances with CBM-CFS3 disturbances
-      disturbanceMeta = rbind(
-        data.frame(eventID = 1, wholeStand = 1, name = "Wildfire"),
-        data.frame(eventID = 2, wholeStand = 1, name = "Clearcut harvesting without salvage"),
-        data.frame(eventID = 3, wholeStand = 0, name = "Generic 20% mortality"),
-        data.frame(eventID = 4, wholeStand = 1, name = "Deforestation"),
-        data.frame(eventID = 5, wholeStand = 0, name = "Generic 20% mortality")
-      ),
-      disturbanceRasters = "https://drive.google.com/file/d/12YnuQYytjcBej0_kdodLchPg7z9LygCt",
+      disturbanceRastersURL = "https://drive.google.com/file/d/12YnuQYytjcBej0_kdodLchPg7z9LygCt",
 
       # Set outputs
       outputs = as.data.frame(expand.grid(
